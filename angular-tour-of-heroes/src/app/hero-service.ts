737f3d52693fca69d2261b5492d36416d850d8b6
akakirde95 <akakirde95@gmail.com>
2020-12-11 12:10:12 +0530

    REST call using HttpClient

--- angular-tour-of-heroes/src/app/hero-service.ts
+++ angular-tour-of-heroes/src/app/hero-service.ts
@@ -3,19 +3,27 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero'
 import { MessageService } from './message-service';
 import { HEROES } from './mock-hero'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
     providedIn:'root'
 })
 export class HeroService{
 
-    constructor(private messageService:MessageService){
+    private heroEndpoint="http://localhost:8080/hero";
+
+    constructor(private messageService:MessageService,private http: HttpClient){
 
     }
 
-    getHeros() : Observable<Hero[]>{
+    /*getHeros() : Observable<Hero[]>{
         this.messageService.add('HeroService:: Fetched HEROS');
         return of(HEROES);
+    }*/
+
+    getHeros() : Observable<Hero[]>{
+        this.messageService.add('HeroService:: Fetched HEROS');
+        return this.http.get<Hero[]>(this.heroEndpoint);
     }
 
     getHero(id: number): Observable<Hero>{
